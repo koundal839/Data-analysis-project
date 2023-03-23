202972c33e0dea02f156367c48b5fc20a39d9d68
koundal839 <110395987+koundal839@users.noreply.github.com>
2023-03-23 16:54:02 +0530

    Add files via upload

---
+++ MYSQL Database files/BikeStores Sample Database - drop all objects.sql
@@ -0,0 +1,24 @@
+/*
+--------------------------------------------------------------------
+© 2017 sqlservertutorial.net All Rights Reserved
+--------------------------------------------------------------------
+Name   : BikeStores
+Link   : http://www.sqlservertutorial.net/load-sample-database/
+Version: 1.0
+--------------------------------------------------------------------
+*/
+
+-- drop tables
+DROP TABLE IF EXISTS sales_order_items;
+DROP TABLE IF EXISTS sales_orders;
+DROP TABLE IF EXISTS production_stocks;
+DROP TABLE IF EXISTS production_products;
+DROP TABLE IF EXISTS production_categories;
+DROP TABLE IF EXISTS production_brands;
+DROP TABLE IF EXISTS sales_customers;
+DROP TABLE IF EXISTS sales_staffs;
+DROP TABLE IF EXISTS sales_stores;
+
+-- drop the schemas
+
+DROP DATABASE IF EXISTS bike_store;
